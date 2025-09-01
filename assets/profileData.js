5bdf578327f7f4cfdef1f862cfd4ca051a852bf3
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-09-01 17:03:16 +0530

    updated about section

--- assets/profileData.js
+++ assets/profileData.js
@@ -5,5 +5,5 @@ export const profileData = {
     experience: 2024,
     github: "https://github.com/theblazingolem",
     x: "https://x.com/blzngdev",
-    about: `I'm a dev who enjoys building small web projects to understand how things work step by step. My goal is to write clear maintainable code and keep improving with every project I take on.`,
+    about: `I'm a dev who enjoys building web projects to understand how things work. My goal is to write clear maintainable code and keep improving with every project I take on.`,
 };
