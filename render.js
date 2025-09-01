590f4afa72083db6379e91ecb3e909cee98ea14e
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-09-01 16:18:20 +0530

    added data files etc

--- render.js
+++ render.js
@@ -1,10 +1,12 @@
 import { skillsData } from "./assets/skillsData.js";
 import { profileData } from "./assets/profileData.js";
 import { projectsData } from "./assets/projectsData.js";
+import { expData } from "./assets/expData.js";
 
 const skillsContainer = document.getElementById("skills");
 const profileContainer = document.getElementById("profile");
 const projectsContainer = document.getElementById("projects");
+const expContainer = document.getElementById("exp");
 
 function renderBadge(skill, iconOnly = false) {
     if (iconOnly) {
@@ -28,37 +30,54 @@ profileContainer.innerHTML = `
       <img class="avatar" src="assets/avatar.png" />
         <h4 class="name imp">${profileData.name}</h4>
         <p class="op-5">${profileData.designation}</p>
-        <div class='flex'><div class="loc">
-          <img src='./svgs/pin.svg' />
-          <span style="padding-top: 2px">${profileData.location}</span>
-        </div>
+        ${
+            profileData.location
+                ? `<div class='flex'><div class="loc">
+                    <img src='./svgs/pin.svg' />
+                    <span style="padding-top: 2px">${profileData.location}</span>
+                   </div>`
+                : ""
+        }
         <div class="grad">
           <img src='./svgs/gre.svg' />
           <span style="padding-top: 2px">${profileData.graduationDate}</span>
         </div></div>
+        <div class="grad">
+          <img src='./svgs/cal.svg' />
+          <span style="padding-top: 2px">${
+              new Date().getFullYear() - profileData.experience
+          }+ years Experience</span>
+        </div></div>
         <button class='git-btn'>Get in Touch</button>
+        <div class='social-links'>
+          <a href="${
+              profileData.github
+          }"><img src="svgs/github.svg" alt="" /></a>
+          <a href="${profileData.x}"><img src="svgs/x.svg" alt="" /></a>
+        </div>
         `;
 
-const projectsArr = projectsData.map(
-    (project) => `
-      <div class="project-item">
-        <div class='prj'>
+projectsContainer.innerHTML = projectsData
+    .map(
+        (project) => `
+      <div class="card-item">
+        <div class='sp-btw'>
           <h4 class="imp">${project.name}</h4>
           <div class='tech'>${project.tech
               .map((sk) => renderBadge(sk, true))
               .join("")}</div>
         </div>
         <p>${project.desc}</p>
-        <div class="project-footer">
+        <div class="card-footer">
           <p>${project.year}</p>
           <div>
             ${
-                project.repoLink
+                project?.repoLink
                     ? `<a href="${project.repoLink}"><img src="svgs/github.svg" alt="" /></a>`
                     : ""
             }
             ${
-                project.viewLink
+                project?.viewLink
                     ? `<a href="${project.viewLink}"><img src="svgs/popup.svg" alt="" /></a>`
                     : ""
             }
@@ -66,5 +85,34 @@ const projectsArr = projectsData.map(
         </div>
       </div>
   `
-);
-projectsContainer.innerHTML = projectsArr.join("");
+    )
+    .join("");
+
+expContainer.innerHTML = expData
+    .map(
+        (item) => `
+      <div class="card-item">
+        <div class='sp-btw'>
+          <h4 class="imp">${item.as}</h4>
+          <p class='tech'>at ${item.at}</p>
+        </div>
+        <p>${item.desc}</p>
+        <div class="card-footer">
+          <p>${item.from} - ${item.to}</p>
+          <div>
+            ${
+                item?.repoLink
+                    ? `<a href="${item.repoLink}"><img src="svgs/github.svg" alt="" /></a>`
+                    : ""
+            }
+            ${
+                item?.viewLink
+                    ? `<a href="${item.viewLink}"><img src="svgs/popup.svg" alt="" /></a>`
+                    : ""
+            }
+          </div>
+        </div>
+      </div>
+  `
+    )
+    .join("");
